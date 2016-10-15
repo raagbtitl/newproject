c3df89d5196f879a783fd7c9afc54034263eb7f2
rahul agarwal <vibes1002003@gmail.com>
2016-10-15 21:12:26 +0530

    Create gitcode.sh

---
+++ gitcode.sh
@@ -0,0 +1,35 @@
+§ Github
+
+git clone https://github.com/raagbtitl/newproject.git
+
+cd newproject
+
+git add .
+
+git commit -m "Add new repos"
+
+git remote
+
+git show 
+
+git add ./Part1
+
+git commit -m "add new folder"
+
+git pull origin master 
+
+git push origin master
+
+git remote shown origin
+
+git submodule add https://github.com/raagbtitl/omics-crispr.git
+
+cd omics-crispr
+
+git add .
+
+git commit -m "add another respos"
+
+git push origin master
+
+git checkout master
