2c43341d182b996faef68ff247e5a1f989754a8b
rah agar <rah agar>
2016-10-15 22:11:58 +0530

    ...

---
+++ gitcode.sh
@@ -0,0 +1,49 @@
+§ Github
+
+git clone https://github.com/raagbtitl/newproject.git 
+
+cd newproject
+
+git add .
+
+git commit -m "Add new repos"
+
+git status -s
+
+git remote
+
+git show 
+
+git config --list
+
+git add ./Part1
+
+git commit -m "add new folder"
+
+git pull origin master 
+
+git push origin master
+
+git remote shown origin
+
+git submodule add https://github.com/raagbtitl/omics-crispr.git
+
+cd omics-crispr
+
+git add .
+
+git commit -m "add another respos"
+
+git push origin master
+
+git checkout master
+
+git log
+
+git remove -v
+
+git tag  v1.4
+
+git push origin --tags
+
+
